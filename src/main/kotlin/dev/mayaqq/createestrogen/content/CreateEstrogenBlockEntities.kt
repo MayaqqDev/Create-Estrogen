aea1c12e434504b94acb726eac284568cd454830
Mayaqq <i@mayaqq.dev>
2026-08-13 14:10:31 -0400

    fix: Crash with flywheel modifying mods

--- src/main/kotlin/dev/mayaqq/createestrogen/content/CreateEstrogenBlockEntities.kt
+++ src/main/kotlin/dev/mayaqq/createestrogen/content/CreateEstrogenBlockEntities.kt
@@ -1,11 +1,11 @@
 package dev.mayaqq.createestrogen.content
 
-import dev.mayaqq.createestrogen.CreateEstrogen
 import dev.mayaqq.createestrogen.MOD_ID
 import dev.mayaqq.createestrogen.client.content.blockRenderers.centrifuge.CentrifugeRenderer
 import dev.mayaqq.createestrogen.client.content.blockRenderers.centrifuge.CreateEstrogenRenderer
 import dev.mayaqq.createestrogen.client.platforms.ofVisual
 import dev.mayaqq.createestrogen.content.blockEntities.CentrifugeBlockEntity
+import dev.mayaqq.estrogen.content.visual
 import invoke.kitty.kritter.registry.api.Registrar
 import invoke.kitty.kritter.registry.blockEntity.blockEntity
 import invoke.kitty.kritter.registry.blockEntity.renderer
@@ -19,7 +19,7 @@ object CreateEstrogenBlockEntities : Registrar<BlockEntityType<*>> by Registrar(
         validBlock(CreateEstrogenBlocks::Centrifuge)
         renderer(::CentrifugeRenderer)
         clientOnly {
-            visual(CreateEstrogenRenderer.CENTRIFUGE_COG::ofVisual, { false })
+            visual(CreateEstrogenRenderer.CENTRIFUGE_COG::ofVisual) { false }
         }
     }
 }
--- src/main/kotlin/dev/mayaqq/createestrogen/content/Transgenders.kt
+++ src/main/kotlin/dev/mayaqq/createestrogen/content/Transgenders.kt
@@ -12,19 +12,6 @@ import invoke.kitty.kritter.utils.clientOnly
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.material.Fluids
 
-// Block entities
-// these need to be inline/crossinline for server-side safety
-inline fun <BE : BlockEntity> BlockEntityBuilder<BE>.visual(crossinline factory: (VisualizationContext, BE, Float) -> BlockEntityVisual<in BE>, noinline predicate: (BE) -> Boolean = { true }) {
-    clientOnly {
-        onRegister {
-            val builder = SimpleBlockEntityVisualizer.builder(it)
-                .factory { ctx, be, f -> factory(ctx, be, f) }
-            predicate.let { builder.skipVanillaRender(it) }
-            builder.apply()
-        }
-    }
-}
-
 typealias FluidContainer = CommonStorage<FluidResource>
 
 operator fun FluidContainer.iterator() = object : Iterator<StorageSlot<FluidResource>> {
